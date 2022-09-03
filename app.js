65fd6ee66bd22c8567f5c748745beaa52be3dafc
Superstar27 <91425528+Superstar27@users.noreply.github.com>
2022-09-03 22:54:33 +0530

    koko

--- app.js
+++ app.js
@@ -23,193 +23,193 @@ mongoose.connect(mongoURI, {
 app.get('/', (req, res) => {
     res.send('Hello World Aniket!')
   })
-  app.post('/register', async (req, res) => {
-    console.log(req.body);
-    const { password, name, phone, email,address,query } = req.body;
+//   app.post('/register', async (req, res) => {
+//     console.log(req.body);
+//     const { password, name, phone, email,address,query } = req.body;
 
-    if(!password || !name || !phone ||!address||!query) {
-        return res.send({
-            status: 400,
-            message: "Missing data from 1",
-            data: req.body
-        })
-    }
+//     if(!password || !name || !phone ||!address||!query) {
+//         return res.send({
+//             status: 400,
+//             message: "Missing data from 1",
+//             data: req.body
+//         })
+//     }
 
     
     
 
-    if(password.length > 11) {
-        return res.send({
-            status: 400,
-            message: "Invalid password",
-            data: req.body
-        })
-    }
-if(query.length && address.length <5){
-    return res.send({
-        status: 400,
-        message: "Invalid query or address",
-        data: red.body
-    })
-}
-    // Write into DB
-        try {
-            let email =req.body.email;
+//     if(password.length > 11) {
+//         return res.send({
+//             status: 400,
+//             message: "Invalid password",
+//             data: req.body
+//         })
+//     }
+// if(query.length && address.length <5){
+//     return res.send({
+//         status: 400,
+//         message: "Invalid query or address",
+//         data: red.body
+//     })
+// }
+//     // Write into DB
+//         try {
+//             let email =req.body.email;
 
-            let formDs = await FormModel.findOne({email:email})
-            console.log(email);
-            if(formDs) {
-                return res.send({
-                    status: 401,
-                    message: "email already taken please use another name"
-                })
-            }
-        }
-catch(err){
-    console.log(err)
-   return res.send({
-        status: 400,
-        message: "Database  seeeee error",
-        error: err
+//             let formDs = await FormModel.findOne({email:email})
+//             console.log(email);
+//             if(formDs) {
+//                 return res.send({
+//                     status: 401,
+//                     message: "email already taken please use another name"
+//                 })
+//             }
+//         }
+// catch(err){
+//     console.log(err)
+//    return res.send({
+//         status: 400,
+//         message: "Database  seeeee error",
+//         error: err
         
-    })
-}
-let formData = new FormModel({
-    name: name,
-    password: password,
-    phone: phone,
-    query:query,
-    address:address
+//     })
+// }
+// let formData = new FormModel({
+//     name: name,
+//     password: password,
+//     phone: phone,
+//     query:query,
+//     address:address
     
 
-})
+// })
 
-if(email)
-    formData.email = email;
-    try {
+// if(email)
+//     formData.email = email;
+//     try {
     
-        let formDb = await formData.save();
+//         let formDb = await formData.save();
 
-        console.log(formDb);
+//         console.log(formDb);
 
-        res.send({
-            status: 200,
-            message: "Form Submmitted Successfully",
-            pata: formDb
-        });
-    }
-    catch(err) {
-        console.log(err)
-        res.send({
-            status: 400,
-            message: "Database error hai deho",
-            error: err
-        })
-    }
-})
-app.post('/update', async(req, res) => {
-    let name = req.body.name;
-    let newData = req.body.newData;
+//         res.send({
+//             status: 200,
+//             message: "Form Submmitted Successfully",
+//             pata: formDb
+//         });
+//     }
+//     catch(err) {
+//         console.log(err)
+//         res.send({
+//             status: 400,
+//             message: "Database error hai deho",
+//             error: err
+//         })
+//     }
+// })
+// app.post('/update', async(req, res) => {
+//     let name = req.body.name;
+//     let newData = req.body.newData;
 
-    try {
+//     try {
 
-        let oldData = await FormModel.findOneAndUpdate({name: name}, newData);
-console.log(newData)
-        res.send({
-            status: 200,
-            message: "Updated data successfully",
-            data: oldData
-        })
-    }
-    catch(err) {
-        res.send({
-            status: 400,
-            message: "Database Error",
-            error: err
-        })
-    }
-  })
-  app.post('/login', async (req,res) => {
-    let email = req.body.email;
-    let pass=req.body.password
-    console.log(pass)
+//         let oldData = await FormModel.findOneAndUpdate({name: name}, newData);
+// console.log(newData)
+//         res.send({
+//             status: 200,
+//             message: "Updated data successfully",
+//             data: oldData
+//         })
+//     }
+//     catch(err) {
+//         res.send({
+//             status: 400,
+//             message: "Database Error",
+//             error: err
+//         })
+//     }
+//   })
+//   app.post('/login', async (req,res) => {
+//     let email = req.body.email;
+//     let pass=req.body.password
+//     console.log(pass)
 
-    let search
-try {
-     search= await FormModel.findOne({email:email})
-    if(!search){
-        return res.send({
-            message:"enter email"
-        })
-    }
+//     let search
+// try {
+//      search= await FormModel.findOne({email:email})
+//     if(!search){
+//         return res.send({
+//             message:"enter email"
+//         })
+//     }
    
-    if (!search.email==email){
-        return res.send({
-            message:"login with correct name",
-            status:400
-        })
-    }
+//     if (!search.email==email){
+//         return res.send({
+//             message:"login with correct name",
+//             status:400
+//         })
+//     }
    
     
-}
-    catch(err){
-        return res.send({
-            message:"see problem"
-        })
-    }
-try {
-    console.log(pass)
-    if(pass==search.password){
-        return res.send({
-            message:"login successfully",
-            status:"200",
-            email:email,
-            data:search
-        })
-    }
-    else{
-        return res.send({
-            message:"enter right password",
-            status:400
-        })
-    }
+// }
+//     catch(err){
+//         return res.send({
+//             message:"see problem"
+//         })
+//     }
+// try {
+//     console.log(pass)
+//     if(pass==search.password){
+//         return res.send({
+//             message:"login successfully",
+//             status:"200",
+//             email:email,
+//             data:search
+//         })
+//     }
+//     else{
+//         return res.send({
+//             message:"enter right password",
+//             status:400
+//         })
+//     }
 
     
 
-}
-catch(err){
-    console.log(err)
-    return res.send({
-        message:"err"
-    })
-}
-})
+// }
+// catch(err){
+//     console.log(err)
+//     return res.send({
+//         message:"err"
+//     })
+// }
+// })
 
-app.get('/dash', async(req, res) => {
-    let email = req.body.email;
-    let search
-    try {
-         search= await FormModel.findOne({email:email})
-        if(!search){
-            return res.send({
-                message:"enter email"
-            })
-        }
-        else{
-            return res.send({
-                message:"great api",
-                data:search
-            })
-        }}
-        catch(err){
-            console.log(err)
-            return res.send({
-                message:"err"
-            })
-        }
+// app.get('/dash', async(req, res) => {
+//     let email = req.body.email;
+//     let search
+//     try {
+//          search= await FormModel.findOne({email:email})
+//         if(!search){
+//             return res.send({
+//                 message:"enter email"
+//             })
+//         }
+//         else{
+//             return res.send({
+//                 message:"great api",
+//                 data:search
+//             })
+//         }}
+//         catch(err){
+//             console.log(err)
+//             return res.send({
+//                 message:"err"
+//             })
+//         }
     
 
-})
+// })
     app.listen(port, () => {
     console.log(`Example app listening  aniket at http://localhost:${port}`)
   })
