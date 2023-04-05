aa72289b4fc203b3fb07207b33afe876e1c3ad2f
Superstar27 <91425528+Superstar27@users.noreply.github.com>
2023-04-05 23:16:48 +0530

    lo

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 var cors = require('cors')
 var validator = require("email-validator");
 
-
+const jwt = require('jsonwebtoken');
 const FormModel = require('./FormModel.js');
 app.use(express.json()); // middleware
 app.use(express.urlencoded({extended: true})); 
@@ -12,7 +12,7 @@ app.use(cors())
 const port = process.env.PORT || 3001
 const mongoURI = 'mongodb+srv://aniket:1q2w3e4r5t@cluster0.2dal9.mongodb.net/bck?retryWrites=true&w=majority';
 
-
+const secret = 'your_secret_key';
 mongoose.connect(mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -21,6 +21,34 @@ mongoose.connect(mongoURI, {
      
     console.log('Connected to  congo database');
   })
+
+
+  function generateToken(email) {
+    const payload = {
+     
+        id: user.id,
+        email: user.email,
+    };
+    const options = { expiresIn: '1d' };
+
+    return jwt.sign(payload, secret, options);
+  }
+  
+  // This function will authenticate a user based on the JWT token
+  function authenticate(req, res, next) {
+    const token = req.headers.authorization?.split(' ')[1]; // Get token from Authorization header
+    if (!token) {
+      return res.status(401).json({ message: 'No token provided' });
+    }
+    try {
+      const decoded = jwt.verify(token, secret);
+      req.user = decoded;
+      next();
+    } catch (error) {
+      return res.status(401).json({ message: 'Invalid token' });
+    }
+  }
+
 
 app.get('/', (req, res) => {
     res.send('Hello World Aniket!')
@@ -137,62 +165,97 @@ console.log(newData)
         })
     }
   })
-  app.post('/login', async (req,res) => {
-    let email = req.body.email;
-    let pass=req.body.password
-    console.log(pass)
+//   app.post('/login', async (req,res) => {
+//     let email = req.body.email;
+//     let pass=req.body.password
+//     console.log(pass)
 
-    let search
-try {
-     search= await FormModel.findOne({email:email})
-    if(!search){
-        return res.send({
-            message:"enter email"
-        })
-    }
+//     let search
+// try {
+//      search= await FormModel.findOne({email:email})
+//     if(!search){
+//         return res.send({
+//             message:"enter email"
+//         })
+//     }
    
-    if (!search.email==email){
-        return res.send({
-            message:"login with correct name",
-            status:400
-        })
-    }
+//     if (!search.email==email){
+//         return res.send({
+//             message:"login with correct name",
+//             status:400
+//         })
+//     }
    
     
-}
-    catch(err){
-        return res.send({
-            message:"see problem"
-        })
-    }
-try {
-    console.log(pass)
-    if(pass==search.password){
-        return res.send({
-            message:"login successfully",
-            status:"200",
-            email:email,
-            data:search
-        })
-    }
-    else{
-        return res.send({
-            message:"enter right password",
-            status:400
-        })
-    }
+// }
+//     catch(err){
+//         return res.send({
+//             message:"see problem"
+//         })
+//     }
+// try {
+//     console.log(pass)
+//     if(pass==search.password){
+//         return res.send({
+//             message:"login successfully",
+//             status:"200",
+//             email:email,
+//             data:search
+//         })
+//     }
+//     else{
+//         return res.send({
+//             message:"enter right password",
+//             status:400
+//         })
+//     }
 
     
 
-}
-catch(err){
-    console.log(err)
-    return res.send({
-        message:"err"
-    })
-}
-})
+// }
+// catch(err){
+//     console.log(err)
+//     return res.send({
+//         message:"err"
+//     })
+// }
+// })
+app.get('/protected', authenticate, (req, res) => {
+    res.send({ message: `Welcome ${req.user.email}!`,
+     status:"200"
+});
+  });
+  
+  // This is an example login route that generates a JWT token
+  app.post('/login', (req, res) => {
+    // You should validate the user's credentials before generating the token
+    const Email="prakashaniket3@gmail.com"
+     const Password='12345678'
+    const { email, password } = req.body;
+    const user = {
+        id: 1,
+        email: email,
+      
+      };
 
+  // Check if email and password were provided in the request body
+  if (!email || !password) {
+    return res.status(400).json({ message: 'Please provide email and password' });
+  }
+
+  // Find the user by email address in your database or data source
+ 
+
+  // If user doesn't exist or password doesn't match, return error
+  if (email!=Email || password!== Password) {
+    console.log(email,password)
+    return res.status(401).json({ message: 'Invalid email or password' });
+  }
+
+  // Generate a JWT token and return it as a response
+  const token = generateToken(user);
+  res.json({ token });
+  });
 app.get('/dash', async(req, res) => {
     let email = req.body.email;
     let search
